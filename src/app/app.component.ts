3f6d8bac8a1b052feecc8ac024189f74e1b72db1
Gourab <smlgourab@gmail.com>
2024-05-11 03:03:01 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {select, Store} from "@ngrx/store";
+import {Observable} from "rxjs";
+import {map} from 'rxjs/operators';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngrx-store-demo';
+export class AppComponent implements OnInit {
+
+    loading = true;
+
+    constructor(private router: Router) {
+
+    }
+
+    ngOnInit() {
+
+      this.router.events.subscribe(event  => {
+        switch (true) {
+          case event instanceof NavigationStart: {
+            this.loading = true;
+            break;
+          }
+
+          case event instanceof NavigationEnd:
+          case event instanceof NavigationCancel:
+          case event instanceof NavigationError: {
+            this.loading = false;
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      });
+
+    }
+
+    logout() {
+
+    }
+
 }
